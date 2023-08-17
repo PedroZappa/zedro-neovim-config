b35390c34ce006cb13e7f6f9fc9420e031152cba
PedroZappa <pedrogzappa@gmail.com>
2023-08-17 10:43:53 +0100

    Init Neovim (.lua) Config backup

---
+++ README.md
@@ -0,0 +1,1 @@
+# zedro-neovim-config
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.showcmd = true
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.wrap = true
+vim.opt.autoindent = true
+vim.opt.autowrite = true
+vim.opt.autoread = true
